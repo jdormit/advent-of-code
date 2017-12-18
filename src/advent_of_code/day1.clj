965881e1153dd31174f7a7e2bd1bf6a2e5b3c9c8
Jeremy Dormitzer <jdormitzer@hubspot.com>
2017-12-18 13:58:50 -0500

    Implement day1/part2

--- src/advent_of_code/day1.clj
+++ src/advent_of_code/day1.clj
@@ -14,9 +14,31 @@
 
 (defn str->nums [str] (map read-string (string/split str #"")))
 
-(defn part1 [args]
+(defn part2-helper [[head & tail] full-list current-index last-num acc]
+  (let [halfway-around-list (nth full-list
+                                 (mod (+ current-index
+                                         (/ (count full-list) 2))
+                                      (count full-list)))]
+    (if (nil? head)
+      (if (= last-num halfway-around-list)
+        (+ acc last-num)
+        acc)
+      (part2-helper tail full-list (inc current-index) head (if (= head halfway-around-list)
+                                                             (+ acc head)
+                                                             acc)))))
+
+(defn part2-main [nums]
+  (part2-helper nums nums 0 (first nums) 0))
+
+(defn run [part-func args]
   (let [nums (-> args
                  (string/join)
                  (str->nums))
-        sum (part1-main nums)]
+        sum (part-func nums)]
     (println sum)))
+
+(defn part1 [args]
+  (run part1-main args))
+
+(defn part2 [args]
+  (run part2-main args))
