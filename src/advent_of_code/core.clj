3d621deee9d0e202c5f07bc9bc53d2fdf362e777
Jeremy Dormitzer <jdormitzer@hubspot.com>
2017-12-18 11:40:08 -0500

    Implement sum-of-matches for match next digit

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -1,7 +1,25 @@
 (ns advent-of-code.core
+  (:require [clojure.string :as string])
   (:gen-class))
+
+(defn sum-of-matches-helper [[head & tail] first-num last-num acc]
+  (if (nil? head)
+    (if (= first-num last-num)
+      (+ acc last-num)
+      acc)
+    (sum-of-matches-helper tail first-num head (+ acc (if (= head (first tail))
+                                                    head
+                                                    0)))))
+(defn sum-of-matches [nums]
+  (sum-of-matches-helper nums (first nums) (first nums) 0))
+
+(defn str->nums [str] (map read-string (string/split str #"")))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [nums (-> args
+                 (string/join)
+                 (str->nums))
+        sum (sum-of-matches nums)]
+    (println sum)))
